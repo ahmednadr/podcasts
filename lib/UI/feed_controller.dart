17fcfea11e6bc59f9512864339da56f4d9749718
ahmednadr <ahmednader.an5@gmail.com>
2023-02-07 12:13:45 +0200

    rm comments

--- lib/UI/feed_controller.dart
+++ lib/UI/feed_controller.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:podcasts/UI/scan.dart';
@@ -34,35 +32,6 @@ class ThreadFeedController {
         image: "http://${ips.last}:5000" + (pod["image"]!),
       ));
     }
-    // List<Podcast> lst = [
-    //   Podcast(
-    //       n: "How will AI change the world we know today?",
-    //       text: "10 mins",
-    //       url: link,
-    //       image: "http://${ips.last}:5000/image/ai.png"),
-    //   // image:
-    //   //     "https://firebasestorage.googleapis.com/v0/b/adg-forums-4644c.appspot.com/o/posts%2FScreenshot%202022-12-10%20at%2011.37.33%20PM.png?alt=media&token=0506014e-b309-4c23-8e85-c5c3dbe89d36"),
-    //   Podcast(
-    //       n: "Follow Your Passion is Terrible Advice. Here's Why.",
-    //       text: "10 mins",
-    //       url: link,
-    //       image: "http://${ips.last}:5000/image/passion.png"),
-    //   // image: "https://www.linkpicture.com/q/passion_1.png"),
-    //   Podcast(
-    //       n: "Do you really need 8 hours of sleep every single day?",
-    //       text: "12 mins",
-    //       url: link,
-    //       image: "http://${ips.last}:5000/image/sleep.png"),
-    //   // image:
-    //   //     "https://firebasestorage.googleapis.com/v0/b/adg-forums-4644c.appspot.com/o/posts%2FScreenshot%202022-12-10%20at%2011.38.29%20PM.png?alt=media&token=e320bb1c-91d5-43b2-afbb-8d9e8bcf8f1e"),
-    //   Podcast(
-    //       n: "A day in the life of a billionaire.",
-    //       text: "12 mins",
-    //       url: link,
-    //       image: "http://${ips.last}:5000/image/life%20of%20money.png"),
-    //   // image:
-    //   //     "https://firebasestorage.googleapis.com/v0/b/adg-forums-4644c.appspot.com/o/posts%2Flife%20of%20money.png?alt=media&token=f1e6231c-a948-45a2-8bfb-9b181484177b")
-    // ];
 
     yield lst;
   }
